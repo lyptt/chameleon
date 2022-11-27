d35e8616594709dde1e6e3697df93674e98ae38f
lyptt <contact@lyptt.uk>
2022-11-26 20:04:55 -0600

    #44 - added missing data to user federated feed query

--- src/db/fetch_user_federated_feed.sql
+++ src/db/fetch_user_federated_feed.sql
@@ -1,9 +1,19 @@
-SELECT DISTINCT e.event_type as event_type, p.* FROM events e
+SELECT DISTINCT e.event_type as event_type, p.*, u.handle AS user_handle, u.fediverse_id AS user_fediverse_id, u.avatar_url AS user_avatar_url, COUNT(DISTINCT l.like_id) AS likes, COUNT(DISTINCT l2.like_id) >= 1 AS liked, COUNT(DISTINCT c.comment_id) AS comments FROM events e
 INNER JOIN posts p
 ON p.post_id = e.post_id
+INNER JOIN users u
+ON u.user_id = p.user_id
+LEFT OUTER JOIN likes l
+ON l.post_id = p.post_id
+LEFT OUTER JOIN likes l2
+ON l2.post_id = p.post_id
+AND l2.user_id = u.user_id
+LEFT OUTER JOIN comments c
+ON c.post_id = p.post_id
 WHERE e.source_user_id = $1
 AND e.target_user_id IS NULL
 AND e.visibility IN ('public_federated', 'public_local')
+GROUP BY e.event_type, p.post_id, u.handle, u.fediverse_id, u.avatar_url
 ORDER BY p.created_at DESC
 LIMIT $2
 OFFSET $3
