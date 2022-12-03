dff5699040ef82c591284e8bdc05bb973df50c50
lyptt <contact@lyptt.uk>
2022-12-03 16:21:35 -0600

    added support for programmatically registering oauth2 apps

---
+++ db/V8__Alter_apps.sql
@@ -0,0 +1,10 @@
+DROP CONSTRAINT fk_app_user;
+
+ALTER TABLE apps DROP COLUMN user_id;
+ALTER TABLE apps DROP COLUMN owner_instance_uri;
+ALTER TABLE apps ALTER COLUMN client_id TYPE VARCHAR(64);
+ALTER TABLE apps ALTER COLUMN client_secret TYPE VARCHAR(64);
+
+CREATE UNIQUE INDEX apps_client_id_secret ON apps(client_id, client_secret);
+CREATE UNIQUE INDEX apps_client_id ON apps(client_id);
+CREATE UNIQUE INDEX apps_client_secret ON apps(client_secret);
--- src/db/app_repository.rs
+++ src/db/app_repository.rs
@@ -12,6 +12,7 @@ use mockall::automock;
 #[async_trait]
 pub trait AppRepo {
   async fn fetch_by_client_id(&self, client_id: &str) -> Result<Option<App>, LogicErr>;
+  async fn create(&self, app: &App) -> Result<(), LogicErr>;
 }
 
 pub type AppPool = Arc<dyn AppRepo + Send + Sync>;
@@ -35,5 +36,21 @@ impl AppRepo for DbAppRepo {
       .map_err(map_db_err)?;
 
     Ok(app)
+  }
+
+  async fn create(&self, app: &App) -> Result<(), LogicErr> {
+    sqlx::query("INSERT INTO apps (app_id, name, description, owner_name, owner_uri, redirect_uri, client_id, client_secret) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)")
+      .bind(app.app_id)
+      .bind(&app.name)
+      .bind(&app.description)
+      .bind(&app.owner_name)
+      .bind(&app.owner_uri)
+      .bind(&app.redirect_uri)
+      .bind(&app.client_id)
+      .bind(&app.client_secret)
+      .execute(&self.db)
+      .await
+      .map(|_| Ok(()))
+      .map_err(map_db_err)?
   }
 }
